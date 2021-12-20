b634581877ce772d05935c5a4d2954a43385f7f5
JBeni <JBeni@users.noreply.github.com>
2021-12-20 18:50:33 +0200

    Add project files.

---
+++ CryptoWebScraper/Controllers/WebScraperController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CryptoWebScraper.Controllers
+{
+    public class WebScraperController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
