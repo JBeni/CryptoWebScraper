e2ce5aeb0697044bf3f95e7207818c57cff5a234
JBeni <JBeni@users.noreply.github.com>
2021-12-21 14:39:44 +0200

    update client App Comp

--- CryptoWebScraper/ClientApp/src/App.js
+++ CryptoWebScraper/ClientApp/src/App.js
@@ -2,10 +2,11 @@ import React, { Component } from 'react';
 import { Route } from 'react-router';
 import { Layout } from './components/Layout';
 import { Home } from './components/Home';
-import { FetchData } from './components/FetchData';
-import { Counter } from './components/Counter';
 
 import './custom.css';
+import SearchCrypto from './components/SearchCrypto';
+import Cryptos from './components/Cryptos';
+import CryptoInfo from './components/CryptoInfo';
 
 export default class App extends Component {
 	static displayName = App.name;
@@ -14,8 +15,9 @@ export default class App extends Component {
 		return (
 			<Layout>
 				<Route exact path="/" component={Home} />
-				<Route path="/counter" component={Counter} />
-				<Route path="/fetch-data" component={FetchData} />
+				<Route path="/cryptos" exact component={Cryptos} />
+				<Route path="/cryptos/:id" component={CryptoInfo} />
+                <Route path="/crypto-chart" component={SearchCrypto} />
 			</Layout>
 		);
 	}
