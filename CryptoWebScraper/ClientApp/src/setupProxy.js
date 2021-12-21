5ffd24f2c08be185c0cdb564b66305e35bbda765
JBeni <JBeni@users.noreply.github.com>
2021-12-21 14:40:04 +0200

    adding route to setup proxy

--- CryptoWebScraper/ClientApp/src/setupProxy.js
+++ CryptoWebScraper/ClientApp/src/setupProxy.js
@@ -7,7 +7,7 @@ const target = env.ASPNETCORE_HTTPS_PORT
 	? env.ASPNETCORE_URLS.split(';')[0]
 	: 'http://localhost:49980';
 
-const context = ['/weatherforecast'];
+const context = ['/WebScraper'];
 
 module.exports = function (app) {
 	const appProxy = createProxyMiddleware(context, {
